020644d6c07fdf3e63aa47daf9487ee5a84154c3
Jonathan Zahedieh <jzahedieh@gmail.com>
2017-03-05 18:38:57 +0000

    Register the root url path to serve the API

--- api/tracker/urls.py
+++ api/tracker/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
 
 urlpatterns = [
+    url(r'^', include(routers.DefaultRouter().urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
